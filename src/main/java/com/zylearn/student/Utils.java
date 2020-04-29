6ff6fcd6d6ad70fcfe609f9d5c372a68161adab7
liuchaosheng <chaoshengl@yeah.net>
2020-04-30 01:02:19 +0800

    二次提交增加了Utils类和其testCase

---
+++ src/main/java/com/zylearn/student/Utils.java
@@ -0,0 +1,12 @@
+package com.zylearn.student;
+
+public class Utils {
+    public static void print(Object object){
+        System.out.print("================Utils Print =====================");
+        System.out.print(object);
+        System.out.print("================Utils Print =====================");
+    }
+    public static int add(int a,int b){
+        return a + b;
+    }
+}
--- src/test/java/com/zylearn/student/AppTest.java
+++ src/test/java/com/zylearn/student/AppTest.java
@@ -7,14 +7,19 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testUtils() {
+        int a = 100;
+        int b = 200;
+        Utils.print(Utils.add(a, b));
     }
 }
